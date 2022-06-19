db70e2a27ba319e5493d3ad2f96a01b492cfa224
livelinksinfo <47534924+livelinksinfo@users.noreply.github.com>
2022-06-19 08:41:12 +0100

    firstcommit

---
+++ hammedCoder/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200),
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date =  models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
